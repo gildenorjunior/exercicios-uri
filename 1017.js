609484a4d475ca25d16f0d3669077ba869dacc04
Junior <junior.silva_costa@live.com>
2021-08-22 20:07:21 -0300

    Exercicios 1017 e 1018

--- 1017.js
+++ 1017.js
@@ -1,3 +1,9 @@
 var input = require("fs").readFileSync("stdin", "utf8");
 var lines = input.split('\n');
 
+var [tempoGasto, velocidadeMedia] = lines;
+
+var totalPercorrido = tempoGasto * velocidadeMedia;
+var qtdCombustivelGasto = totalPercorrido / 12;
+
+console.log(qtdCombustivelGasto.toFixed(3));
